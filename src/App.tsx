a0ff70350782630d6561c6b36ad4c95dc985e96a
Mishan Parajuli <mparajuli1@stu.jsu.edu>
2024-11-12 19:38:26 +0545

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <Pricing />
+        <Testimonials />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
